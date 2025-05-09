104d80feb049d82be859a26fed5656c23cbeb9e5
MartStef <mbs121@ic.ac.uk>
2025-05-09 16:21:37 +0300

    Final download-data.R and metadata.txt

--- src/download-data.R
+++ src/download-data.R
@@ -1,25 +1,14 @@
 library(here)
 
-#if (!dir.exists("data")) dir.create("data")
+# Set paths
+zip_path <- here("data", "raw", "stanford-car-dataset-by-classes-folder.zip")
+extract_dir <- here("data", "raw", "car_data")
 
-#url <- "http://ai.stanford.edu/~jkrause/car196/cars_train.tgz"
-url <- "https://www.kaggle.com/datasets/jutrera/stanford-car-dataset-by-classes-folder/data?select=car_data"
-dest <- here("data", "raw", "car_data")
 
-#if (!file.exists(dest)) {
-#  cat("📥 Downloading dataset...\n")
-#  download.file(url, dest)
-#  cat("📦 Extracting...\n")
-#  untar(dest, exdir = "data/")
-#} else {
-#  cat("✅ Dataset already exists. Skipping download.\n")
-#}
-
-# Only download if not already there
-if (!file.exists(dest)) {
-  cat("📥 Downloading dataset from Kaggle...\n")
-  system("kaggle datasets download -d jutrera/stanford-car-dataset-by-classes-folder -p data/")
+# Download if the zip file doesn't exist
+if (!file.exists(zip_path)) {
+  cat("📥 Downloading dataset...\n")
+  system(paste("kaggle datasets download -d jutrera/stanford-car-dataset-by-classes-folder -p", here("data", "raw")))
 }
 
-# Unzip it
-unzip(dest, exdir = here("data", "raw"))
+unzip(zip_path, exdir = extract_dir)
